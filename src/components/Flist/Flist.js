26bbddb6082a4b3ed85b85a1d89dd8e9bf647d0b
sarefinsag <shamimularefin100@gmail.com>
2020-04-11 14:47:05 +0600

    done done

--- src/components/Flist/Flist.js
+++ src/components/Flist/Flist.js
@@ -4,11 +4,11 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './Flist.css';
 
 const Flist = (props) => {
-    const {image, name, bio, price} = props.friend;
+    const {image, name, bio, price, height, width} = props.friend;
     return (
         <div className="flist">
             <div>
-                <img src={image} alt={name}/>
+                <img src={image} alt={name} height={height} width={width} />
             </div>
             <div className="bio-area">
                 <h4 className="flist-name"> {name} </h4>
--- src/fakeData/friends.js
+++ src/fakeData/friends.js
@@ -3,106 +3,136 @@ var friends = [
                 "name": "Yausub",
                 "bio": "Public Administration",
                 "price": "5000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "1"
+                "image": "https://i.ibb.co/5TsJDZf/ppy.jpg",
+                "id": "1",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Bayzid",
                 "bio": "Computer Science",
                 "price": "7000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "2"
+                "image": "https://i.ibb.co/M2HhjQk/FB-IMG-15865132168606226.jpg",
+                "id": "2",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Sajeebur Rahman",
                 "bio": "Social Work",
                 "price": "4000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "3"
+                "image": "https://i.ibb.co/vhTv2zx/IMG-20200410-160353.jpg",
+                "id": "3",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Arefin Genius",
                 "bio": "Cyber Security",
                 "price": "8000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "4"
+                "image": "https://i.ibb.co/GHHsfTj/IMG-20200410-155616.jpg",
+                "id": "4",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Nahid Hasan",
                 "bio": "Astro Physics",
                 "price": "6000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "5"
+                "image": "https://i.ibb.co/QvhJ2Mh/IMG-20191219-140807.jpg",
+                "id": "5",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Emon Saikot",
                 "bio": "Physics",
                 "price": "5000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "6"
+                "image": "https://i.ibb.co/Krg0jd7/FB-IMG-15865134865843810.jpg",
+                "id": "6",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "AK Shuvo",
                 "bio": "IOT",
                 "price": "6400",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "7"
+                "image": "https://i.ibb.co/vdCj7YW/IMG-6757.jpg",
+                "id": "7",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Emamul Hasan",
                 "bio": "Zoology",
                 "price": "4700",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "8"
+                "image": "https://i.ibb.co/4dLQRp8/FB-IMG-15865132253927569.jpg",
+                "id": "8",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "SH Sobuj",
                 "bio": "Islamic History",
                 "price": "4600",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "9"
+                "image": "https://i.ibb.co/ysTgbgt/IMG-20200410-160542.jpg",
+                "id": "9",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "JH Utsob",
                 "bio": "Physics",
                 "price": "6000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "10"
+                "image": "https://i.ibb.co/DLT2Rbw/IMG-20200319-143345.jpg",
+                "id": "10",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Emon Hasan",
                 "bio": "Architecture",
                 "price": "6000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "11"
+                "image": "https://i.ibb.co/x3zdwvb/IMG-20200411-143903.jpg",
+                "id": "11",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "T Romany",
                 "bio": "Psycology",
                 "price": "7000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "12"
+                "image": "https://i.ibb.co/7S9WFKk/FB-IMG-15865140313331779.jpg",
+                "id": "12",
+                "height":"250px",
+                "width": "250px"
             },
             {
                 "name": "Raisul Ruman",
                 "bio": "Social science",
                 "price": "4500",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "13"
+                "image": "https://i.ibb.co/86d63DD/IMG-20200410-160456.jpg",
+                "id": "13",
+                "height":"250px",
+                "width": "250px"
             },
             {
-                "name": "Shakhawat H",
+                "name": "Shakhawat",
                 "bio": "Management",
                 "price": "4700",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "14"
+                "image": "https://i.ibb.co/k4j4TDy/IMG-20200411-143129.jpg",
+                "id": "14",
+                "height":"250px",
+                "width": "250px"
             },
             {
-                "name": "Mustafiz R",
+                "name": "Mustafiz",
                 "bio": "Mathematics",
                 "price": "7000",
-                "image": "https://i.ibb.co/1m6Mf3C/sag.jpg",
-                "id": "15"
+                "image": "https://i.ibb.co/8zYGp98/IMG-20200411-144302.jpg",
+                "id": "15",
+                "height":"250px",
+                "width": "250px"
             }
             
         ]
